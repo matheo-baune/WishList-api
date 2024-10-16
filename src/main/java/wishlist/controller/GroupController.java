12412cf7f758a88fa5b6e40ad3bf90dc8a0d730e
Mathéo <matheo.baune@gmail.com>
2024-10-16 15:00:52 +0200

    Unused import statement removed

--- src/main/java/wishlist/controller/GroupController.java
+++ src/main/java/wishlist/controller/GroupController.java
@@ -2,7 +2,6 @@ package wishlist.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
